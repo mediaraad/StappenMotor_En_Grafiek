4f15a2f7b0620059cccea3ab33e3bcc9a7d3e0a7
Peter Maasse <development@mediaraad.nl>
2026-02-20 16:20:08 +0100

    vertalen afrikaans eruit  hahahah

--- StppenMotor_En_Grafiek.ino
+++ StppenMotor_En_Grafiek.ino
@@ -33,7 +33,7 @@ bool isPaused = false;
 
 const char* htmlPage PROGMEM = R"rawliteral(
 <!DOCTYPE html>
-<html>
+<html lang="nl">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
